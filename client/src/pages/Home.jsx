c5db47fc55bd3ad6581bfde81cd5cfd49d286b09
PoornaChaitanya <poornachaitanya4259@gmail.com>
2026-05-04 18:10:48 +0530

    add Navbar component

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import Banner from "../components/Banner";
+import Banner from "../components/home/Banner";
 import Hero from "../components/home/Hero";
 import Features from "../components/home/Features";
 import Testimonials from "../components/home/Testimonials";
